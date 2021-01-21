4a6f9aaf5349e9536f64e201ae986d9a1c62da84
iamacarrot92 <limbu_101@outlook.com>
2021-01-21 22:49:44 +0000

    ticTacToe game and update on README file

--- README.md
+++ README.md
@@ -9,9 +9,12 @@
 |3. **Coffee Machine**|A simple machine to make coffee|
 |4. **Loan Calculator**|Calculate different financial parameters|
 |5. **Simple Chat Bot**| A simple chat bot|
-|6. **Tic-Tac-Toe**| coming soon!|
+|6. **Tic-Tac-Toe**| A simple childhood game of Tic-Tac-Toe|
 
 ## Medium
+|Title|Description|
+|-----|-----------|
+|1. **Simple Banking System**| Coming soon!|
 
 
 ## Hard
--- easy/ticTacToe.py
+++ easy/ticTacToe.py
@@ -16,30 +16,46 @@ class TicTacToe:
         if self.values[0][0] + self.values[1][1] + self.values[2][2] == expected or self.values[2][0] + self.values[1][1] + self.values[0][2] == expected:
             return True
 
-    def get_result(self, x, o, count_input):
-        result = ""
-        if (x == True and o == True) or abs(count_input.count('X') - count_input.count('O')) > 1:
-            result = "Impossible"
-        elif x == True:
-            result = "X wins"
-        elif o == True:
-            result = "O wins"
-        elif count_input.count('_') == 0:
-            result = "Draw"
-        else:
-            result = "Game not finished"
-        return result
+    def user_gameplay(self):
+        store = "X"
+        count = 0
+        while True:
+            check_x = self.check_common("XXX")
+            check_o = self.check_common("OOO")
+            user_input = input("Enter the coordinates: ").split()
+            if user_input[0].isdecimal() and user_input[1].isdecimal():
+                x = int(user_input[0])
+                y = int(user_input[1])
+                if x > 3 or y > 3:
+                    print("Coordinates should be from 1 to 3!")
+                elif self.values[x - 1][y - 1] in ["O", "X"]:
+                    print("This cell is occupied! Choose another one!")
+                else:
+                    if store == "X":
+                        self.values[x - 1][y - 1] = "X"
+                        store = "O"
+                        count += 1
+                    else:
+                        self.values[x - 1][y - 1] = "O"
+                        store = "X"
+                        count += 1
+                    print(self.display())
+                    check_x = self.check_common("XXX")
+                    check_o = self.check_common("OOO")
+                    if check_x == True:
+                        print("X wins")
+                        break
+                    elif check_o == True:
+                        print("O wins")
+                        break 
+                    elif count == 9:
+                        print("Draw")
+                        break
+            else:
+                print("You should enter numbers!")
 
-user = input("Enter cells: ")
-board = [[user[0], user[1], user[2]],[user[3], user[4], user[5]],[user[6], user[7], user[8]]]
+user= "_________"
+start_game = [[user[0], user[1], user[2]],[user[3], user[4], user[5]],[user[6], user[7], user[8]]]
 
-gameplay = TicTacToe(board)
-print(gameplay.display())
-
-check_x = gameplay.check_common("XXX")
-check_o = gameplay.check_common("OOO")
-
-result = gameplay.get_result(check_x,check_o, user)
-print(result)
-
-
+result = TicTacToe(start_game)
+result.user_gameplay()
